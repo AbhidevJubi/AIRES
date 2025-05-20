d081d28cd4266e25d09d295dabce15b223246a5f
ABHIDEV JUBI <118052178+AbhidevJubi@users.noreply.github.com>
2025-05-20 14:01:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'dwolt33'
+app.config['MYSQL_PASSWORD'] = ''
 app.config['MYSQL_DB'] = 'resume_screener'
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 
